b38958a5f2e889ea48b87aa0f1c80ad95bc2c7a2
Malorolam <mal2261@gmail.com>
2014-08-26 09:07:18 -0400

    1.1.6 release
    Reduced peat spawn size slightly
    Fixed an issue where fuel conversion bench item creation mode got stuck at 2 items
    Fixed a server crash null pointer exception
    Added ardite, cobalt, Electrical Age tungsten, platinum, and Mekansim osmium ore multiblock furnace recipes

--- mal/core/MalCore.java
+++ mal/core/MalCore.java
@@ -28,7 +28,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid = MalCore.MODID, version = MalCore.VERSION)
 public class MalCore {
     public static final String MODID = "MalCore";
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
     public static final String VersionURL = "https://www.dropbox.com/s/ao6p1iye6w2suec/malcoreVersion.info?dl=1";
 	public static boolean VERBOSEMODE = true;
 	
